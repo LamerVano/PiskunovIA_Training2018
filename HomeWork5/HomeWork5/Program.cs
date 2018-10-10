c43648dae115a24f9c82b0f953e3ea5a03fc2b41
LamerVano <vania_piskunov@tut.by>
2018-10-10 12:01:49 +0300

    Push HomeWork5

---
+++ HomeWork5/HomeWork5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BigNumber bigNumber = new BigNumber(-24);
+            bigNumber = bigNumber.Sum(-80);
+            Console.WriteLine(bigNumber.ToString());
+            bigNumber = bigNumber.Multiply(-10);
+            Console.WriteLine(bigNumber.ToString());
+            bigNumber = bigNumber.Subtraction(83);
+            Console.WriteLine(bigNumber.ToString());
+            bigNumber = bigNumber.Sum(903);
+            Console.WriteLine(bigNumber.ToString());
+            bigNumber = bigNumber.Multiply(bigNumber.ToString());
+            Console.WriteLine(bigNumber.ToString());
+            bigNumber = bigNumber.Multiply(bigNumber.Number, bigNumber.IsSigned);
+            Console.WriteLine(bigNumber.ToString());
+            bigNumber = bigNumber.Subtraction(BigNumber.CalcFactorial(21));
+            Console.WriteLine(bigNumber.ToString());
+            Console.Read();
+        }
+    }
+}

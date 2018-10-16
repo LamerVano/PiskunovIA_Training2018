4dc8d843f1d9bd68b9fad02398ea1ca2b8e6affe
LamerVano <vania_piskunov@tut.by>
2018-10-16 18:52:05 +0300

    Push Task_8

---
+++ Task_8/Task_8/Words.cs
@@ -0,0 +1,30 @@
+﻿namespace Task_8
+{
+    class Words
+    {
+        private Word[] _words;
+
+        public Words(string str)
+        {
+            string[] words = str.Split(' ');
+            int lenght = words.Length;
+            _words = new Word[lenght];
+            for(int i = 0; i < lenght; i++)
+            {
+                _words[i] = new Word(words[i]);
+            }
+        }
+
+        public override string ToString()
+        {
+            string str = "";
+
+            foreach(Word word in _words)
+            {
+                str += word.ToString() + " ";
+            }
+
+            return str;
+        }
+    }
+}

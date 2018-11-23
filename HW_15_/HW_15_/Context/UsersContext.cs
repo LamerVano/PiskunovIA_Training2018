debca95e3b00dd57e0f8e3247baacc8889b8c65c
LamerVano <vania_piskunov@tut.by>
2018-11-23 20:56:35 +0300

    Site Users

---
+++ HW_15_/HW_15_/Context/UsersContext.cs
@@ -0,0 +1,14 @@
+﻿using HW_15_.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace HW_15_.Context
+{
+    public class UsersContext: DbContext
+    {
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ HW_15_/HW_15_/Models/User.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace HW_15_.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string MiddleName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Mail { get; set; }
+        [Required]
+        [MinLength(6)]
+        public string Password { get; set; }
+        [Required]
+        [MinLength(6)]
+        [Compare("Password")]
+        public string CheckPassword { get; set; }
+        [Required]
+        [Range(18, 99)]
+        public int Age { get; set; }
+        [Required]
+        [Phone]
+        public string Phone { get; set; }
+        [Required]
+        [StringLength(20)]
+        public string Address { get; set; }
+    }
+}
